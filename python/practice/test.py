072b85567fbb2b7c7bb608f67ab88b40540aa724
choby <yiweiming@gmail.com>
2016-04-23 11:18:09 +0800

    add skeleton

--- python/practice/test.py
+++ python/practice/test.py
@@ -1,6 +1,25 @@
-from os import path
-
-with open('test.csv') as f:
-    print f.read()
-
-print "end of script"
+class Base1(object):
+    def __init__(self, name, age):
+        self.name = "Base1"
+        self.age = 35
+        
+    def print_name(self):
+        print self.name
+        
+class Base2(object):
+    def __init__(self, name):
+        self.name = "Base2"
+        
+    def print_name(self):
+        print self.name
+        
+class Derived(Base2, Base1):
+    def __init__(self, name):
+        super(Derived, self).__init__(name)
+        self.name = "Derived"
+        
+    # def print_name(self):
+    #     print "Derived"        
+        
+sample_object = Derived("sample_object")
+sample_object.print_name()
