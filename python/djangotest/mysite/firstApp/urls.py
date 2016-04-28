676bba49d45287467b27d71793166a8f3e27dee2
yiweiming-github <yiweiming@gmail.com>
2016-04-28 16:34:52 +0800

    More files

--- python/djangotest/mysite/firstApp/urls.py
+++ python/djangotest/mysite/firstApp/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'firstApp'
 urlpatterns = [    
     url(r'^$', views.index, name='index'),
-    url(r'^convert', views.convert, name='convert'),        
+    url(r'^convert', views.convert, name='convert'),
+    
+    url(r'^q$', views.qindex, name='qindex'),
+    url(r'^q/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^q/(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^q/(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^q/(?P<question_id>[0-9]+)/detail/$', views.detail, name='detail')
 ]
