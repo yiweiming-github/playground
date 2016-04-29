2097284221318809cf0d5380e0e7369d80723a84
yiweiming-github <yiweiming@gmail.com>
2016-04-29 18:15:57 +0800

    Add handlers

--- python/tornadotest/HelloSite/Handlers.py
+++ python/tornadotest/HelloSite/Handlers.py
@@ -10,3 +10,25 @@ class ContentHandler(tornado.web.RequestHandler):
     def get(self):
         items = ['item1', 'item2', 'item3']
         self.render('templates/content.html', title='This is content page', items = items)
+        
+class CallbackStyleHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        http = tornado.httpclient.AsyncHTTPClient()
+        http.fetch("http://xxxx", callback = on_response)
+        
+    def on_response(self, response):
+        if response.error:
+            raise tornado.web.HTTPError(500)
+        json = tornado.escape.join_decode(response.body)
+        self.write(json)
+        self.finish()
+        
+        
+class CoroutineStyleHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        http = tornado.httpclient.AsyncHTTPClient();
+        response = yield http.fetch("http:/xxxx")
+        json = tornado.escape.join_decode(response.body)
+        self.write(json)
