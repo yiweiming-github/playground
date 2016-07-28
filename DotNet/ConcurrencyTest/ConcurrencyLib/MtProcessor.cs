b55165bee2a5faeeee0e27d61c6829148daf4581
yiweiming-github <yiweiming@gmail.com>
2016-07-28 17:42:55 +0800

    add .NET files

---
+++ DotNet/ConcurrencyTest/ConcurrencyLib/MtProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrencyLib
+{
+    public class MtProcessor : IProcessor
+    {
+        public MtProcessor()
+        {
+            
+        }
+
+        public void Process(int taskCount)
+        {
+            for (int i = 0; i < taskCount; ++i)
+            {
+                Start();
+            }
+        }
+
+        public void Start()
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(Finish));
+            ++QueueLength;
+        }
+
+        public void Finish(object obj)
+        {
+            Console.WriteLine("Thread {0} working. length {1}", Thread.CurrentThread.GetHashCode(), QueueLength);
+            for (int i = 0; i < 10000; i++)
+            {
+                var rnd = new Random();
+                rnd.Next(1, 100000);
+            }
+            --QueueLength;
+        }
+
+        public bool AllDone()
+        {
+            return QueueLength == 0;
+        }
+
+        public int QueueLength = 0;
+    }
+
+    
+}
---
+++ DotNet/MachineLearning/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using numl.Model;
+using numl.Supervised.DecisionTree;
+using numl.Supervised.KNN;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var description = Descriptor.Create<Iris>();
+
+            var generator = new KNNGenerator();
+            var data = Iris.Load();
+            var model = generator.Generate(description, data);
+            Console.WriteLine(model);
+            
+
+            var iris = new Iris()
+            {
+                SepalLength = 4.6m,
+                SepalWidth = 3.1m,
+                PetalLength = 1.5m,
+                PetalWidth = 0.2m
+            };
+            var pIris = model.Predict(iris);
+            Console.ReadLine();
+        }
+    }
+}
