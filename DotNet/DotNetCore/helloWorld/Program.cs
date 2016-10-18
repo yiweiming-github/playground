f67e964125f87152d2d1defc7eb4eacbaf511310
weiming <yiweiming@gmail.com>
2016-10-18 09:13:30 +0800

    add a dotnet core sample

---
+++ DotNet/DotNetCore/helloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder().UseKestrel().UseStartup<Startup>().Build();
+            host.Run();
+        }
+    }
+}
