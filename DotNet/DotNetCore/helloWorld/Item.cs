e57dc49f69bfac99621ac541b194dfa2e97402b9
yiweiming@gmail.com <yiweiming@gmail.com>
2019-10-31 13:37:56 +0800

    delete old files

--- DotNet/DotNetCore/helloWorld/Item.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsoleApplication
-{
-    public class Item
-    {
-        public double NumberValue{get;set;}
-        public string StrValue{get;set;}
-        public bool BooleanValue{get;set;}
-    }
-}
